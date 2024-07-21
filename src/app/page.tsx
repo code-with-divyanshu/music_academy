751e8872c2481ddea5954a44257e6fe0b079aca2
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-07-21 20:26:09 +0530

    Add HeroSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.03]"></main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.03]">
+      <HeroSection />
+    </main>
   );
 }
