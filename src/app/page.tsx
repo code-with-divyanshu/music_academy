1e943aaa5efb5199975876282941a3fa824c4b2c
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-07-22 15:13:21 +0530

    Add Fetured Courses Component, Site Title, Disc & Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import FeturedCourses from "@/components/FeturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.03]">
       <HeroSection />
+      <FeturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
