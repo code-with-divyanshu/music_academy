7d91901db340ed8b7ea71cf29661f5e253216d3b
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-07-23 23:30:17 +0530

    Customize Featured Courses

--- src/components/FeturedCourses.tsx
+++ src/components/FeturedCourses.tsx
@@ -33,7 +33,7 @@ function FeturedCourses() {
         </div>
       </div>
       <div className="mt-10">
-        <div className="grid grid-cols-1 sm:grid-col-2 lg:grid-cols-3 gap-8 justify-center">
+        <div className="grid grid-cols-1 sm:grid-col-2 lg:grid-cols-3 gap-8 justify-center px-8">
           {featuredCourses.map((course: Course) => (
             <div key={course.id} className="flex justify-center">
               <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm p-4">
@@ -43,8 +43,8 @@ function FeturedCourses() {
                     alt="jordans"
                     height="200"
                     width="200"
-                    className="object-contain rounded-md"
-                    placeholder="empty"
+                    className="object-contain rounded-md w-auto h-auto"
+                    placeholder="empty" // "empty" | "blur" | "data:image/..."
                   />
                   <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
                     {course.title}
