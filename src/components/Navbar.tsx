fe9c12d1228890b8332260a736169530c8bf4b82
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-07-24 22:00:00 +0530

    Add New Route For Every Course Type & Webinars

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,10 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn(
+        "fixed top-10 inset-x-0 text-center max-w-2xl mx-auto z-50",
+        className
+      )}
     >
       <Menu setActive={setActive}>
         <Link href={"/"}>
@@ -18,15 +21,26 @@ function Navbar({ className }: { className?: string }) {
             item="Home"
           ></MenuItem>
         </Link>
-        <MenuItem setActive={setActive} active={active} item="Our Courses">
+        <MenuItem setActive={setActive} active={active} item="Courses">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/courses">All Courses</HoveredLink>
-            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
-            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
-            <HoveredLink href="/courses">Songwriting</HoveredLink>
-            <HoveredLink href="/courses">Music Production</HoveredLink>
+            <HoveredLink href="/courses/basic">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses/advance">
+              Advanced Composition
+            </HoveredLink>
+            <HoveredLink href="/courses/songwriting">Songwriting</HoveredLink>
+            <HoveredLink href="/courses/musicproduction">
+              Music Production
+            </HoveredLink>
           </div>
         </MenuItem>
+        <Link href={"/webinars"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Webinars"
+          ></MenuItem>
+        </Link>
         <Link href={"/contact"}>
           <MenuItem
             setActive={setActive}
